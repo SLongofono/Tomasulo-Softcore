5846e379268541248b59777dea34718a29b5aafa
Stephen Longofono <8992396+SLongofono@users.noreply.github.com>
2018-10-21 12:07:32 -0400

    Removes redundant STALE flag

--- src/ROB.py
+++ src/ROB.py
@@ -6,7 +6,6 @@ ID = 0
 DEST = 1
 VALUE = 2
 DONEFLAG = 3
-STALE = 4
 
 class ROB():
     """
@@ -26,7 +25,7 @@ class ROB():
     def __init__(self,size):
         if size < 1:
             raise IndexError(f"ROB initialized with invalid size {size}")
-        self.q = [ [-1, "", None, False, True] for x in range(size)]
+        self.q = [ [-1, "", None, True] for x in range(size)]
         self.size = size
         self.head = 0
         self.tail = 0
@@ -36,9 +35,11 @@ class ROB():
         """
         Determines if the ROB is full.
 
-        In this case, full means that there are no stale entries in the buffer.
+        In this case, full means the head and tail both point to an instruction
+        which is not complete.
         """
-        return self.head == self.tail and not self.q[self.head][STALE]
+        return self.head == self.tail and not self.q[self.head][DONEFLAG]
+
 
     def findAndUpdateEntry(self, entryID, value):
         """
@@ -77,7 +78,7 @@ class ROB():
         if self.isFull():
             raise IndexError("The ROB is full!")
         else:
-            self.q[self.tail] = [entryID, destination, None, False, False]
+            self.q[self.tail] = [entryID, destination, None, False]
             ret = f"ROB{self.tail}"
             self.tail += 1
             if self.tail == self.size:
@@ -89,25 +90,24 @@ class ROB():
         """
         Determines if the oldest instruction is complete and ready to commit
         """
-        return self.q[self.head][DONEFLAG] and not self.q[self.head][STALE]
+        return self.q[self.head][DONEFLAG] and self.head != self.tail
 
 
     def commit(self):
         """
         Retrieve the head of the ROB and advance the head beyond it.
 
-        @return True on success, False otherwise
+        @return A copy of the ROB entry as a tuple: (ID, destination, value,
+        doneflag)
         """
         retVal = self.q[self.head].copy()
 
-        # Note: we must mark the item as stale in case we are out of
-        # instructions to process
-        self.q[self.head][STALE] = True
         self.head += 1
         if self.head == self.size:
             self.head = 0
 
         return retVal
+
 
     def dump(self):
         """
