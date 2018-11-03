e224d9efc82583e2909d3198d394340a011c9864
zexalistic <3140104629@zju.edu.cn>
2018-11-03 16:50:29 -0400

    Debugged version

--- src/FPALU.py
+++ src/FPALU.py
@@ -1,130 +1,143 @@
-# @file     FPAdder
-# @authors  Yihao
+# @file     FPALU
+# @authopipeline  Yihao
 
-class FPAdder_waitlist:
+class FPALU_pipeline:
 	"""
-	This class implements the FPAdder waitlist
+	This class implements the FPAdder pipeline
 	"""
 	
 	def __init__(self, pipelineLen):
 		"""
-		Constructor for the FPAdder waitlist class.
+		Constructor for the FPAdder pipeline class.
 
-		@param waitlist An list containing the items in waitlist
-		@param maxlen An integar representing the maximum length of the waitlist
+		@param pipeline An list containing the items in pipeline
+		@param maxlen An integar representing the maximum length of the pipeline
 		"""
-		self.waitlist = []
+		self.pipeline = []
 		self.maxlen = pipelineLen
 
-	def add_list(self, instr_num, shedule, result):
-		if(self.getlen < self.maxlen):
-			self.waitlist.append([instr_num, shedule, result])
-			return True		#True for success
-		else:
-			return False	#False for fail
+	def add(self, instr_num, shedule, result):
+			self.pipeline.append([instr_num, shedule, result])
 	
-	def getlen:
-		return len(self.waitlist)
+	def busy(self):
+		return len(self.pipeline) == self.maxlen
 
-	def check_list(self, time):
+	def check(self, time):
 		'''
-		Check if any instruction in the waitlist can be popped
+		Check if any instruction in the pipeline can be popped
 		'''
 		output = []
-		for i in range(self.getlen):
-			if(self.waitlist[i][1] == time):
-				output.append([waitlist[i][0], waitlist[i][2]])
+		for i in range(len(self.pipeline)):
+			if(self.pipeline[i][1] == time):
+				output.append([self.pipeline[i][0], self.pipeline[i][2]])
 		return output
 
 
-class FPALU:
-    """
-    This class implements a pipelined FPAdder.
+class FPMultiplier:
+	def __init__(self, latency, bufferLen):
+		self.latency = latency
+		self.bufferLen = bufferLen									#max buffer length
+		self.buffer = []
+		self.pipeline = FPALU_pipeline(5)							#set pipeline length as 5
+		self.time = 0
+		self.activeInstruction = None
 
-    The class tracks what it is actively executing, and reports time since
-    instantiation for debugging purposes.  The latency of each instruction is
-    encoded along with the maximum output buffer size at instantiation.  This
-    implies that this unit is non-pipelined.
-    """
+	def execute(self, instr_id, instr, op1, op2):
+		self.activeInstruction = (instr_id, instr, op1, op2)
+		if(instr == 'MUL.d'):
+			result = float(op1 * op2)
+			schedule = self.time + latency['MUL.d']
+			self.pipeline.add(instr_id, schedule, result) 
+		else:
+		    raise ValueError("Unknown operation [ {} ] in FP.adder, time [ {} ]".format(op, self.time))
 
-    def __init__(self, latency, pipelineLen, bufferLen, time):
-        """
-        Constructor for the FPAdder class
+	def busy(self):
+		return ((self.pipeline.busy()) and (len(self.buffer) < self.bufferLen) ) 	
 
-        @param latency An integer value representing the number of cycles
-        required per FP addition.
-		@param pipelineLen An integer value representing the number of pipelines
-		in FP adder
-		@param parallel An integer value representing the number of pipelines
-		that are busy in FP adder
-        @param bufferLen An integer value representing how many results to
-        buffer on output before stalling further inputs
+	def isResultReady(self):
+		return len(self.buffer) > 0
 
-        @param waitlist A two dimension list stores the result instructions executing 
-        in the pipeline and its complete time. For each member, the first parameter
-        stands for the time to complete and the second represents the results. e.g.
-        waitlist[0][0] = time_to_complete, waitlist[0][1] = result_of_calculation
-        """
-        self.latency = latency
-        self.bufferLen = bufferLen
-        self.buffer = []
-        self.waitlist = FPAdder_waitlist(pipelineLen)
-		self.time = time
+	def getResult(self):
+		return self.buffer.pop(0)
 
-	def execute(self, instr_id, instr, op1, op2,  time):
-			if(instr == 'MUL'):
-				result = float(op1 * op2)
-				schedule = time + latency['MUL']
-			elif(instr == 'DIV'):
-				result = float(op1 / op2)
-				schedule = time + latency['DIV']
-			elif(instr == 'ADD'):
-				result = float(op1 + op2)
-				schedule = time + latency['ADD']
-			elif(instr == 'SUB'):
-				result = float(op1 / op2)
-				schedule = time + latency['SUB']
+	def advanceTime(self):
+		self.time += 1
+		output = self.pipeline.check(self.time) 		
+		self.buffer += output
+	
+	def dump(self):
+		print("FP Multiplier".ljust(44, '=').rjust(80,'='))
+		print("Time:\t\t\t{}".format(self.time))
+		print("Busy:\t\t\t{}".format(self.busy()))
+		print("Instruction:\t\t{}".format(self.activeInstruction))
+		print("Output Buffer Contents:")
+		for item in self.buffer:
+			print("\tID:{}, Value:{}".format(item[0],item[1]))
+		print()
+				
+class FPAdder:
+	def __init__(self, latency, bufferLen):
+		self.latency = latency
+		self.bufferLen = bufferLen									#max buffer length
+		self.buffer = []
+		self.pipeline = FPALU_pipeline(5)						#set pipeline length as 5
+		self.time = 0
+		self.activeInstruction = None
 
-			if(self.waitlist.addlist(instr_id, schedule, result) == FALSE):
-				return FALSE	#return busy
-			elif:
-				return TRUE
+	def execute(self, instr_id, instr, op1, op2):
+		self.activeInstruction = (instr_id, instr, op1, op2)
+		if(instr == 'ADD.d'):
+			result = float(op1 + op2)
+			schedule = self.time + latency['ADD.d']
+			self.pipeline.add(instr_id, schedule, result) 
+		elif(instr == 'SUB.d'):
+			result = float(op1 - op2)
+			schedule = self.time + latency['SUB.d']
+			self.pipeline.add(instr_id, schedule, result) 
+		else:
+		    raise ValueError("Unknown operation [ {} ] in FP.adder, time [ {} ]".format(op, self.time))
 
-    def isResultReady(self):
-        """
-        Getter determines if a result is waiting to be written back
-        """
-        return len(self.buffer) > 0
+	def busy(self):
+		return self.pipeline.busy() and len(self.buffer) < self.bufferLen 	
 
-    def getResult(self):
-        """
-        Getter for oldest execution result
+	def isResultReady(self):
+		return len(self.buffer) > 0
 
-        @return A tuple with the instruction ID followed by the result
-        """
-        return self.buffer.pop(0)
+	def getResult(self):
+		return self.buffer.pop(0)
 
-    def advanceTime(self):
-        """
-        Advances the time for this unit and propogates results to output buffer as they complete
-        """
-        self.time += 1
-        if self.waitlist.getlen > 0:
-        	#if the waitlist is not empty
-          	output = check_list(self.time) 		
-			self.buffer += output
-			if(len(self.buffer) > self.bufferLen):
-				print('outrange')
-				self.buffer -= output
+	def advanceTime(self):
+		self.time += 1
+		output = self.pipeline.check(self.time) 		
+		self.buffer += output
+
+	def dump(self):
+		print("FP Adder".ljust(44, '=').rjust(80,'='))
+		print("Time:\t\t\t{}".format(self.time))
+		print("Busy:\t\t\t{}".format(self.busy()))
+		print("Instruction:\t\t{}".format(self.activeInstruction))
+		print("Output Buffer Contents:")
+		for item in self.buffer:
+			print("\tID:{}, Value:{}".format(item[0],item[1]))
+		print()
 
 if __name__ == "__main__":
-    time = 0
-	latency = {'MUL': 10, 'DIV': 16, 'ADD': 4, 'SUB' : 4}
-    myALU = FPALU(latency,5,5,time)
-    myALU.execute("ADD",1, 33, -1, 1)
-    myALU.advanceTime()
-    print(f"Result ready?:{myALU.isResultReady()}")
-    myALU.advanceTime()
-    print(f"Result ready?:{myALU.isResultReady()}")
-    r = myALU.getResult()
-    print(r)
+	t = 0
+	latency = {'MUL.d': 5, 'ADD.d': 3, 'SUB.d': 3}
+	myALU = FPAdder(latency, 3)
+	print(myALU.busy)
+	myALU.dump()
+	myALU.execute(10,"ADD.d", 33, -1)
+	myALU.dump()
+	myALU.advanceTime()
+	myALU.dump()
+	print("Result ready?:{}".format(myALU.isResultReady()))
+	myALU.advanceTime()
+	myALU.advanceTime()
+	myALU.advanceTime()
+	print("Result ready?:{}".format(myALU.isResultReady()))
+	r = myALU.getResult()
+	print(r)
+	myALU.dump()
+
+
