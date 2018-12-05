855eb00d5ada5248a064b154390f1f5eee41d477
Stephen Longofono <8992396+SLongofono@users.noreply.github.com>
2018-12-04 20:13:30 -0500

    Fixes UML path problem

--- documentation/makeUML.sh
+++ documentation/makeUML.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 echo "Generating UML..."
 
-pyreverse3 ../Tomasulo.py
-dot -Tpng classes.dot>./UML/Tomasulo.png
+cd ..
 
-pyreverse3 ../src/IntegerALU.py
-dot -Tpng classes.dot>./UML/IntegerALU.png
+pyreverse3 Tomasulo.py
+dot -Tpng classes.dot> documentation/UML/Tomasulo.png
 
-pyreverse3 ../src/InstructionQueue.py
-dot -Tpng classes.dot>./UML/InstructionQueue.png
+pyreverse3 src/IntegerALU.py
+dot -Tpng classes.dot> documentation/UML/IntegerALU.png
 
-pyreverse3 ../src/ROB.py
-dot -Tpng classes.dot>./UML/ROB.png
+pyreverse3 src/InstructionQueue.py
+dot -Tpng classes.dot> documentation/UML/InstructionQueue.png
 
-pyreverse3 ../src/FPALU.py
-dot -Tpng classes.dot>./UML/FPALU.png
+pyreverse3 src/ROB.py
+dot -Tpng classes.dot> documentation/UML/ROB.png
 
-pyreverse3 ../src/MemoryUnit.py
-dot -Tpng classes.dot>./UML/MemoryUnit.png
+pyreverse3 src/FPALU.py
+dot -Tpng classes.dot> documentation/UML/FPALU.png
 
-pyreverse3 ../src/RAT.py
-dot -Tpng classes.dot>./UML/RAT.png
+pyreverse3 src/MemoryUnit.py
+dot -Tpng classes.dot> documentation/UML/MemoryUnit.png
 
-pyreverse3 ../src/ARF.py
-dot -Tpng classes.dot>./UML/ARF.png
+pyreverse3 src/RAT.py
+dot -Tpng classes.dot> documentation/UML/RAT.png
 
-pyreverse3 ../src/BranchUnit.py
-dot -Tpng classes.dot>./UML/BranchUnit.png
+pyreverse3 src/ARF.py
+dot -Tpng classes.dot> documentation/UML/ARF.png
 
-pyreverse3 -AS ../Tomasulo.py
-dot -Tpng classes.dot>./UML/Hierarchy.png
+pyreverse3 src/BranchUnit.py
+dot -Tpng classes.dot> documentation/UML/BranchUnit.png
+
+pyreverse3 -AS Tomasulo.py
+dot -Tpng classes.dot> documentation/UML/Hierarchy.png
 
 rm *.dot
 
